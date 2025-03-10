436f2c04f948d8eb692ca4507c63a2f871ac6b54
kunal89204 <kunalkhandelwal108@gmail.com>
2025-03-10 16:42:19 +0530

    updated

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ const connectDB = require("./src/db/connectDB")
 const PORT = process.env.PORT || 8000
 
 
+connectDB()
 
 
-
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`)
-    })
-}).catch((error) => {
-    console.log(error)
-    return error
+app.use('/api/v1', (req, res) => {
+    res.json("hello wolrld")
 })
 
 
-
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
 
